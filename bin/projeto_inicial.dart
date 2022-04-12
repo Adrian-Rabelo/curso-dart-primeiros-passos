0bff49fbe311ba53e7d98f2db7cbb2df1950fc39
Adrian Rabelo <adrianrabeloe10@gmail.com>
2022-04-12 18:12:32 -0400

    entendendo booleano

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,8 +1,11 @@
 void main() {
   int idade = 26;
-  double altura = 780e6; // -> significa 780 elevado à sexta potência
-  print(
-      "Sua altura: $altura\n" +
-      "Sua idade: $idade"
-  );
+  double altura = 1.86; // -> significa 780 elevado à sexta potência
+  bool geek = true;
+  bool isIgual =
+      (idade == altura); // é possível verificar condições dentro de variáveis
+  print("Sua altura: $altura\n" +
+      "Sua idade: $idade\n" +
+      "Sua idade é igual sua altura? $isIgual\n"
+          "Você é geek? $geek");
 }
