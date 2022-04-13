928b727c8150f49ea332920b9929b15378de2053
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-13 12:20:55 -0400

    entendendo while/do while loop

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,11 +1,12 @@
 void main() {
-  int idade = 20 ;
+  int idade = 20;
   double altura = 1.71; // -> significa 780 elevado à sexta potência
   bool geek = true;
   bool isIgual =
       (idade == altura); // é possível verificar condições dentro de variáveis
   const String nome = 'Ádrian';
   bool maiorDeIdade;
+  int energia = 100;
 
   String apelido = 'Rabelo';
   String frase = "Eu sou $nome meu apelido é $apelido\n" +
@@ -31,13 +32,23 @@ void main() {
     maiorDeIdade = false;
   }
 
-  for (int i=1;i<8;i++) {
+  for (int i = 1; i < 8; i++) {
     print('Concluí $i voltas');
   }
+
+  // while (energia>0) {
+  //   print('Mais uma Repetição');
+  //   energia = energia - 6;
+  // }
+
+  do {
+    print("Mais uma volta");
+    energia = energia - 6;
+  } while (energia > 0);
 
   String frase2 = "Eu sou ${adrame[3]} meu apelido é ${adrame[4]}\n" +
       "Minha altura é ${adrame[1]}, tenho ${adrame[0]} anos\n" +
       "Me considero geek? ${adrame[2]}\n"
-      "Eu sou maior de idade? $maiorDeIdade";
+          "Eu sou maior de idade? $maiorDeIdade";
   print(frase2);
 }
