012d345f887e414dfaadc5dc6adbb3493f4a4ced
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-13 11:39:29 -0400

    entendendo listas

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,6 +1,6 @@
 void main() {
-  int idade = 26;
-  double altura = 1.86; // -> significa 780 elevado à sexta potência
+  int idade = 20 ;
+  double altura = 1.71; // -> significa 780 elevado à sexta potência
   bool geek = true;
   bool isIgual =
       (idade == altura); // é possível verificar condições dentro de variáveis
@@ -10,5 +10,20 @@ void main() {
       "Minha altura é $altura, tenho $idade anos\n" +
       "Me considero geek? $geek\n"
           "Sua idade é igual sua altura? $isIgual";
-  print(frase);
+
+  // LISTAS --
+  List<String> listaNomes = [
+    'Ricarth',
+    'Natália',
+    'Alex',
+    'Ândriu',
+    'André',
+  ];
+
+  List<dynamic> adrame = [20, 1.71, true, 'Ádrian', 'Rabelo'];
+
+  String frase2 = "Eu sou ${adrame[3]} meu apelido é ${adrame[4]}\n" +
+      "Minha altura é ${adrame[1]}, tenho ${adrame[0]} anos\n" +
+      "Me considero geek? ${adrame[2]}\n";
+  print(frase2);
 }
